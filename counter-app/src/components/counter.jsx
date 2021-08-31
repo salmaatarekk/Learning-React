b8a1d1bc6c76b366360c6e0168a1922e36488ef2
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-08-31 19:08:02 +0200

    making two buttons and change the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,15 +2,53 @@
 import React, { Component } from 'react';
 class Counter extends Component {
 
+    state = {
+        count: 0,
+        // imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
+    };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: 'thin'
+    };
+    //constructor() {
+    //super();
+    //  this.handleIncrement = this.handleIncrement.bind(this);
+    //}
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    handleDecrement = () => {
+        this.setState({ count: this.state.count - 1 });
+    }
     render() {
 
         return (
-            <React.Fragment>
-                <h1>Hello World</h1>
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                <span style={this.styles} className={this.getBadgeClasses()} >{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm">Decrement</button>
+
+            </div>
         );
+    }
+
+
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
 export default Counter;
+
+// In span i can add any valid JavaScrpit Code
