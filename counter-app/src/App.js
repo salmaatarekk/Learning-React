5bb5f750fe44ae4de472162f83e6af073a6dd393
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-09-04 00:13:48 +0200

    fixing bugs

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,17 +1,66 @@
-import './App.css';
-import NavBar from './components/navbar';
+import React, { Component } from 'react';
 import Counters from './components/counters';
-import React from 'react';
+import NavBar from './components/navbar';
 
-function App() {
-  return (
-    <React.Fragment>
-   <NavBar/>
-     <main className = "container">
-       <Counters/>
-     </main>
-   </React.Fragment>
-  );
+class App extends Component {
+  
+  state = {
+    counters: [
+        {id: 1, value: 4},
+        {id: 2, value: 0},
+        {id: 3, value: 0},
+        {id: 4, value: 0}
+        
+    ]
+};
+handleDelete = counterId => {
+    console.log('Deleting...', counterId);
+    const counters = this.state.counters.filter(c => c.id != counterId);
+    this.setState({counters});
+
+};
+handleReset = () => {
+    const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+    });
+    this.setState({counters});
+
+};
+handleIncrement = (counter) =>{
+    const counters = [...this.state.counters];
+    const idx = counters.indexOf(counter);
+    counters[idx] = {...counter};
+    counters[idx].value++;
+    this.setState({counters});
+
+};
+handleDecrement = (counter) =>{
+    const counters = [...this.state.counters];
+    const idx = counters.indexOf(counter);
+    counters[idx] = {...counter};
+    counters[idx].value--;
+    this.setState({counters});
+
 }
 
+  render() { 
+    return (
+      <React.Fragment>
+      <NavBar />
+      <main className = "container">
+        <Counters
+        counters = {this.state.counters}
+        onReset = {this.handleReset}
+        onIncrement = {this.handleIncrement}
+        onDecrement = {this.handleDecrement}
+        onDelete = {this.handleDelete}
+        />
+      </main>
+      </React.Fragment>
+
+      );
+  }
+}
+ 
 export default App;
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -1,57 +1,18 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 class Counters extends Component {
-    state = {
-        counters: [
-            {id: 1, value: 4},
-            {id: 2, value: 0},
-            {id: 3, value: 0},
-            {id: 4, value: 0}
-            
-        ]
-    };
-    handleDelete = counterId => {
-        console.log('Deleting...', counterId);
-        const counters = this.state.counters.filter(c => c.id != counterId);
-        this.setState({counters});
-
-    };
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({counters});
-
-    };
-    handleIncrement = (counter) =>{
-        const counters = [...this.state.counters];
-        const idx = counters.indexOf(counter);
-        counters[idx] = {...counter};
-        counters[idx].value++;
-        this.setState({counters});
-    
-    };
-    handleDecrement = (counter) =>{
-        const counters = [...this.state.counters];
-        const idx = counters.indexOf(counter);
-        counters[idx] = {...counter};
-        counters[idx].value--;
-        this.setState({counters});
-
-    };
     
 
     render() {
         return (
             <div>
-                <button onClick = {this.handleReset} className="btn bnt-primary btn-sm m-2">Reset</button>
-               {this.state.counters.map(counter => 
+                <button onClick = {this.props.onReset} className="btn bnt-primary btn-sm m-2">Reset</button>
+               {this.props.counters.map(counter => 
                <Counter
                 key = {counter.id}
-                onDelete = {this.handleDelete}
-                onIncrement = {this.handleIncrement}
-                onDecrement = {this.handleDecrement}
+                onDelete = {this.props.onDelete}
+                onIncrement = {this.props.onIncrement}
+                onDecrement = {this.props.onDecrement}
                 counter = {counter}
                 >
                    <h4>
