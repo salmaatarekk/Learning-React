96e492e436a42d70706b44a7e79807e9931cef5a
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-09-04 14:16:03 +0200

    disable button based on condition

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -26,14 +26,21 @@ class Counter extends Component {
         return (
 
             
-            <div>
-                {this.props.children }
+            <div className = "row"> 
+            <div className="col-2">
                 <span style={this.styles} className={this.getBadgeClasses()} >{this.formatCount()}</span>
-                <button onClick={() => {this.props.onIncrement(this.props.counter)}} className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+            <div className="col">
 
-                <button onClick={() => this.props.onDecrement(this.props.counter) } className="btn btn-secondary btn-sm">Decrement</button>
+                {this.props.children }
+                <button onClick={() => {this.props.onIncrement(this.props.counter)}} className="btn btn-secondary btn-sm m-2">+</button>
 
-                <button onClick = { () => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDecrement(this.props.counter) } className="btn btn-secondary btn-sm m-2"
+                disabled = {this.props.counter.value === 0 ? 'disabled' : ''}
+                >-</button>
+
+                <button onClick = { () => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm ">Delete</button>
+            </div>
             </div>
         );
     }
